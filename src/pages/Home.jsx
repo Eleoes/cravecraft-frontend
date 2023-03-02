7459ee7b4a2bec788a55ae12c6d2cdeb279ac54b
eleoes <leo.escaroz@gmail.com>
2023-03-02 15:24:25 -0800

    wip: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <main> 
             <span>Welcome to</span>
-            <h1>Crave Craft</h1>
+            <h1 class="logo">Crave Craft</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas illum est atque. Nulla facilis perferendis ut, omnis eligendi consectetur totam. Minus, quo culpa! Ut numquam libero provident mollitia iure? Aspernatur!</p>
             <button class="primary">Get Started</button>
         </main>
