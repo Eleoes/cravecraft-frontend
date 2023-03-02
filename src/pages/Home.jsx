67566b09a11938f7add0645c9da109f6a04cc067
eleoes <leo.escaroz@gmail.com>
2023-03-02 11:23:09 -0800

    added global styles, wip: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <main> 
+            <span>Welcome to</span>
+            <h1>Crave Craft</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas illum est atque. Nulla facilis perferendis ut, omnis eligendi consectetur totam. Minus, quo culpa! Ut numquam libero provident mollitia iure? Aspernatur!</p>
+            <button class="primary">Get Started</button>
+        </main>
     )
 }
 
