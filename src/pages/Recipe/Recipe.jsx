30f46e69fea815d281c40e059c4f7bc455360ae2
eleoes <leo.escaroz@gmail.com>
2023-03-16 12:26:30 -0700

    wip: Render recipe

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Recipe = () => {
-    
+const Recipe = (props) => {
+    const { id } = useParams();
+    const recipes = props.recipes;
+    const recipe = recipes ? recipes.find((p) => p._id === id) : null;
+
+    const loaded = () => {
+        return (
+            <>
+            <h1>{recipe.title}</h1>
+            </>
+        )
+    }
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
     return (
-        <div>Recipe</div>
+        <div>
+            { recipe ? loaded() : loading() }
+        </div>
     )
 }
 
