598ae523794b429db1c80dab3eb47bcdfc4f35d9
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2023-03-28 12:59:48 -1000

    recipe classnames for styling refractored

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -9,37 +9,37 @@ function Recipes({ recipes }) {
         <Link to="/create">
           <button className="recipe__button primary">Create </button>
         </Link>
-        <div className="portfolio__filters">
-          <div className="portfolio__container container grid">
+        <div className="recipes__filters">
+          <div className="recipes__container container grid">
             {recipes.map((recipe) => {
               return (
-                <div className="project__card" key={recipe._id}>
+                <div className="recipes__card" key={recipe._id}>
                   <Link to={`/recipes/${recipe._id}`}>
                     <img
                       src={recipe.image}
-                      alt="project image"
-                      className="project__img"
+                      alt="recipes image"
+                      className="recipes__img"
                     />
                   </Link>
                   <Link to={`/recipes/${recipe._id}`}>
-                    <h3 className="project__title">{recipe.title}</h3>
+                    <h3 className="recipes__title">{recipe.title}</h3>
                   </Link>
-                  <div className="project__links">
+                  <div className="recipes__links">
                     <a
                       href={recipe._id}
                       target="_blank"
-                      className="project__button"
+                      className="recipes__button"
                     >
                       Ingredients{" "}
-                      <i className="bx bx-right-arrow-alt project__button-icon"></i>
+                      <i className="bx bx-right-arrow-alt recipes__button-icon"></i>
                     </a>
                     <a
                       href={recipe._id}
                       target="_blank"
-                      className="project__button"
+                      className="recipes__button"
                     >
                       Directions{" "}
-                      <i className="bx bx-right-arrow-alt project__button-icon"></i>
+                      <i className="bx bx-right-arrow-alt recipes__button-icon"></i>
                     </a>
                   </div>
                 </div>
